6eef48cd5091057cfc994855929d51287b69e8c6
developerfab <fab7696550@gmail.com>
2020-05-02 13:41:45 -0500

    refactor code

--- server.rb
+++ server.rb
@@ -1,29 +1,36 @@
-require 'sinatra'
-require "sinatra/json"
 require 'active_record'
-require 'sinatra/activerecord'
 require 'json'
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'sinatra/json'
 
 require './service'
 
 set :database, "sqlite3:db/services.sqlite3"
 
+before do
+  message = request.body.read
+  @params = JSON.parse(message).with_indifferent_access if message.present?
+end
+
 get '/services' do
-  services = Service.all.to_json
-  json JSON.parse(services)
+  services = Service.all
+  json services
 end
 
 post '/services' do
-  params = JSON.parse(request.body.read).with_indifferent_access
-  service = Service.create({ name: params[:name] })
-  json JSON.parse(service.to_json)
+  service = Service.new({ name: @params[:name] })
+  if service.save
+    json service
+  else
+    json service.errors
+  end
 end
 
 delete '/services' do
-  params = JSON.parse(request.body.read).with_indifferent_access
-  service = Service.where(id: params[:id]).last
+  service = Service.where(id: @params[:id]).last
   if service.destroy
-    json JSON.parse(service.to_json)
+    json service
   else
     json service.errors
   end
--- service.rb
+++ service.rb
@@ -1,2 +1,3 @@
 class Service < ActiveRecord::Base
+  validates :name, presence: true
 end
